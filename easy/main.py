21b345e0b43aa529c7147d76652a615796f8553b
napaintga <rina.stka@gmail.com>
2024-11-23 10:32:40 +0200

    +

--- easy/main.py
+++ easy/main.py
@@ -1,112 +1,102 @@
 from PyQt5.QtCore import QEvent
-from PyQt5.QtWidgets import QApplication,QInputDialog,QMessageBox
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QApplication, QInputDialog, QMessageBox, QMainWindow, QFileDialog
 from PyQt5.QtGui import QPixmap
 from ui import Ui_MainWindow
-
-from PIL import Image
-from PIL import ImageFilter
-from PIL import ImageEnhance
-from PyQt5.QtWidgets import QFileDialog
+from PIL import Image, ImageFilter, ImageEnhance
 import os
 
 class Widget(QMainWindow):
-    def   __init__(self):
+    def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.image = None
-
-        self.image = Image.open("images.jfif")
-        self.update_image()
+        self.workdir = None
         self.ui.btn_left.clicked.connect(self.rotate_left)
         self.ui.btn_right.clicked.connect(self.rotate_right)
         self.ui.btn_flip.clicked.connect(self.flip_image)
         self.ui.btn_bw.clicked.connect(self.bw_image)
         self.ui.btn_sharp.clicked.connect(self.sharpen_image)
+        self.ui.btn_dir.clicked.connect(self.show_files)
+        self.ui.listWidget.itemClicked.connect(self.show_pictures)
 
-
-        self.ui.listWidget = i
-
-
-
-
-    def update_image(self,image):
+    def update_image(self, image=None):
         self.ui.label.hide()
-        if image:
-            pixmap = QPixmap(image)
-            self.image.save("pepe.jpg")
-            pixmap = QPixmap("pepe.jpg")
-            w,h =self.ui.label.width(),self.ui.label.height()
-            pixmap = pixmap.scaled(w,h)
-            self.ui.label.setPixmap(pixmap)
-            self.ui.label.show()
+        pixmap = QPixmap(image)
+        w, h = self.ui.label.width(), self.ui.label.height()
+        pixmap = pixmap.scaled(w, h)
+        self.ui.label.setPixmap(pixmap)
+        self.ui.label.show()
 
-    
     def rotate_left(self):
         self.image = self.image.rotate(90)
-        self.update_image()
+        self.image.save("copy.png")
+        self.update_image("copy.png")
 
     def rotate_right(self):
         self.image = self.image.rotate(-90)
-        self.update_image()
+        self.image.save("copy.png")
+        self.update_image("copy.png")
 
     def flip_image(self):
         self.image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
-        self.update_image()
+        self.image.save("copy.png")
+        self.update_image("copy.png")
 
     def bw_image(self):
-        self.image = self.image.convert('1')
-        self.update_image()
+        self.image = self.image.convert("L")
+        self.image.save("copy.png")
+        self.update_image("copy.png")
 
     def sharpen_image(self):
         self.image = self.image.filter(ImageFilter.SHARPEN)
+        self.image.save("copy.png")
+        self.update_image("copy.png")
 
     def choose_dir(self):
-        global workdir
-        workdir = QFileDialog.getExistingDirectory(self)
+        self.workdir = QFileDialog.getExistingDirectory(self)
+        if not self.workdir:
+            QMessageBox.warning(self, "Увага", "Директорію не вибрано!")
 
-        print("workdir ") 
-
-    
-    def filter(self,files, ext):
+    def filter(self, files, ext):
         res = []
         for file in files:
-            for e in ext:
-                if file.endswith(e):
-                    res.append(file)
+            if any(file.lower().endswith(e.lower()) for e in ext):
+                res.append(file)
         return res
 
-    def show_fles(self):
-        ext = ["png ", "jpg", "PNG","JPG" ]
+    def show_files(self):
+        ext = ["png", "jpg", "jpeg", "bmp", "gif"]
         self.choose_dir()
-        if workdir:
-            all_fills = os.listdir(workdir)
-            print(all_fills)
-            filter_files = self.filter(all_fills, ext)
-            print(filter_files)
+        if self.workdir:
+            all_files = os.listdir(self.workdir)
+            filter_files = self.filter(all_files, ext)
             self.ui.listWidget.clear()
             for file in filter_files:
                 self.ui.listWidget.addItem(file)
 
+    def show_pictures(self):
+        if self.ui.listWidget.currentRow() != -1:
+            try:
+                name = self.ui.listWidget.currentItem().text()
+                path = os.path.join(self.workdir, name)
+                self.image = Image.open(path)
+                self.update_image(path)
+            except Exception as e:
+                QMessageBox.critical(self, "Помилка", f"Неможливо відкрити файл: {e}")
+
     def save_image(self):
         if self.image:
-            save_part, = QFileDialog.getSaveFileName(self,"зберегти файл",workdir,"Images(*.png*jpeg*jpg.bmp*.gif)")
-            if save_part:
-                self.image.save(save_part)
-                QMessageBox.information(self,"успіх","зображення успішно збережено")
+            save_path, _ = QFileDialog.getSaveFileName(
+                self, "Зберегти файл", self.workdir, "Images (*.png *.jpg *.jpeg *.bmp *.gif)"
+            )
+            if save_path:
+                self.image.save(save_path)
+                QMessageBox.information(self, "Успіх", "Зображення успішно збережено!")
             else:
-                QMessageBox.information(self,"помилка","не вибрано місце збереження")
+                QMessageBox.warning(self, "Увага", "Файл для зображення не вибрано.")
         else:
-            QMessageBox.information(self,"Увага","Помилка зображення для зображення")
-
-        
-
-
-
-    
-
-    
+            QMessageBox.warning(self, "Увага", "Немає зображення для збереження.")
 
 app = QApplication([])
 ex = Widget()
